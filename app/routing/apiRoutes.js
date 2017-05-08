a143bc992e7e168bd69109f0cbc18887da26128a
lasperilla <l.asperilla@gmail.com>
2017-05-08 13:00:24 -0400

    Update apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,3 +16,22 @@ module.exports = function (app) {
                 }
                 );
     });
+
+    for (friends.length){
+        var friendScores = friends[i].scores;
+        var bestMatchVariance = null;
+        var bestMatchID = null;
+        
+        function calcDiffFunc = function(userscore, friendscore) { 
+        for (userscore.length) {
+            var diffArr = []
+            diffArr.push(Math.abs(userscore[i]-friendscore[i]);
+            var sum = diffArr.reduce(function(a, b) { return a + b; }, 0);
+            return sum;
+        };//end for userscore
+        };//end calcdiff func 
+         
+        var friendVariance = calcDiffFunc(user.scores, friends[i].scores);
+        
+            if (!bestMatchVariance || friendVariance < bestMatchVariance) {
+                bestMatchID = 
