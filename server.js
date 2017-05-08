c7d5da2e72874db417c32047bd0f6f14d76aeadc
lasperilla <l.asperilla@gmail.com>
2017-05-08 12:14:09 -0400

    Update server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
