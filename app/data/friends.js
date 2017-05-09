e14b0798dd30d8487a46c4fb03046aabf73ce80c
lasperilla <l.asperilla@gmail.com>
2017-05-09 13:06:55 -0400

    got friend variance logic down. need to work on sending res

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,155 @@
 var friendsArray = [
   {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+    "name": "Friend",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
     ]
-}
+  }, {
+    "name": "Friend2",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        5,
+        2,
+        2,
+        2,
+        5,
+        2,
+        2,
+        2,
+        2,
+        1
+    ]
+  }, {
+    "name": "Friend3",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        1,
+        4,
+        3,
+        5,
+        3,
+        4,
+        1,
+        5,
+        4,
+        3
+    ]
+  }, {
+    "name": "Friend4",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        3,
+        4,
+        4,
+        4,
+        2,
+        1,
+        3,
+        5,
+        4,
+        3
+    ]
+  }, {
+    "name": "Friend5",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        5,
+        4,
+        5,
+        5,
+        2,
+        1,
+        2,
+        3,
+        1,
+        1
+    ]
+  }, {
+    "name": "Friend6",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        1,
+        1,
+        3,
+        5,
+        3,
+        1,
+        1,
+        1,
+        2,
+        3
+    ]
+  }, {
+    "name": "Friend7",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        2,
+        1,
+        4,
+        3,
+        5,
+        5,
+        3,
+        3,
+        2,
+        5
+    ]
+  }, {
+    "name": "Friend8",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        1,
+        1,
+        4,
+        4,
+        4,
+        2,
+        3,
+        3,
+        1,
+        5
+    ]
+  }, {
+    "name": "Friend9",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        4,
+        2,
+        2,
+        3
+    ]
+  }, {
+    "name": "Friend10",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        4,
+        3,
+        4,
+        1,
+        2,
+        1,
+        3,
+        3,
+        3,
+        5
+    ]
+  }, 
 ];
 
 // export the array
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,40 +1,46 @@
 const friends = require("../data/friends.js");
 
-module.exports = function (app) {
+module.exports = function(app) {
     // API GET Requests
-    app.get("/api/friends", function (req, res) {
+    app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
-    
-    // API POST Requests
-    app.post("/api/friends", function (req, res) {
-            friends.push(req.body);
-            res.json(
-                {
-                    message: 'successfully added profile.'
-                    //todo: respond with best match obj here?
-                }
-                );
-    });
 
-    for (friends.length){
-        var friendScores = friends[i].scores;
-        var bestMatchVariance = null;
-        var bestMatchID = null;
-        
-        function calcDiffFunc = function(userscore, friendscore) { 
-        for (userscore.length) {
-            var diffArr = []
-            diffArr.push(Math.abs(userscore[i]-friendscore[i]);
-            var sum = diffArr.reduce(function(a, b) { return a + b; }, 0);
-            return sum;
-        };//end for userscore
-        
-        };//end calcdiff func 
-         
-        var friendVariance = calcDiffFunc(user.scores, friends[i].scores);
-        
-            if (!bestMatchVariance || friendVariance < bestMatchVariance) {
+    // API POST Requests
+    app.post("/api/friends", function(req, res) {
+        var user = req.body;
+        let bestMatchVariance = null;
+        let bestMatchID = null;
+        for (var i = 0; i < friends.length; i++) {
+            var friendScores = friends[i].scores;
+
+
+            function calcDiffFunc(userscore, friendscore) {
+                var diffArr = [];
+                for (var j = 0; j < userscore.length; j++) {
+                    diffArr.push(Math.abs(userscore[j] - friendscore[j]));
+                };
+                var sum = diffArr.reduce(function(a, b) {
+                    return a + b;
+                }, 0);
+                return sum;
+            }; //end calcdiff func
+
+            let friendVariance = calcDiffFunc(user.scores, friends[i].scores);
+            console.log('friendVariance' + i)
+            console.log(friendVariance)
+
+            if (!bestMatchID || friendVariance < bestMatchVariance) {
                 bestMatchID = i;
+                console.log('best match is now' + bestMatchID)
                 bestMatchVariance = friendVariance;
-            }
+            };
+        };
+
+        console.log('your best friend is ' + friends[bestMatchID].name)
+        res.json({
+            message: 'successfully added profile.'
+                //todo: respond with best match obj here?
+        });
+    });
+};
